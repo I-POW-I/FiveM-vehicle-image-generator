562835ae8972a9349d0ff8330868ec6480b9a2c7
POW <146384484+I-POW-I@users.noreply.github.com>
2025-11-23 01:14:31 +0000

    Update config.lua

--- vehicle-image-generator/config.lua
+++ vehicle-image-generator/config.lua
@@ -5,30 +5,27 @@ Config = {}
 Config.DefaultWebhook = '' -- Set a default webhook here to pre-fill the UI input field
 
 -- ** Vehicle Spawn Codes ** --
--- List your vehicle model names here.
+-- List your vehicle spawn codes here (labels will be auto-generated from the spawn code)
 Config.VehicleSpawnCodes = {
     
     "gbargento7f", "gbbanshees", "gbcometcl", "gbcomets1t", "gbcomets2r",
     "gbirisz",
 }
 
-
 -- ** Camera Settings **--
 Config.CameraSettings = {
-    coords = vector3(-772.2191, 768.9974, 213.1987), -- Where vehicles spawn
-    heading = 0.0, -- Vehicle heading
-    cameraOffset = vector3(-3.0, 8.0, 1.4), -- Camera position, front-left elevated
-    cameraRotation = vector3(-15.0, 0.0, 215.0), -- Camera angle, for front 3/4 view
-    fov = 37.0 -- Field of view
+    coords = vector3(-27.0600, -1095.9000, 27.6000), -- Where vehicles spawn
+    heading = 143.4, -- Vehicle heading
+    cameraOffset = vector3(-2.4, -8.2, 1.5), -- Camera position, front-left elevated
+    cameraRotation = vector3(0.0, 0.0, -16.8), -- Camera angle, for front 3/4 view
+    fov = 38.2 -- Field of view
 }
 
 -- ** Screenshot Settings ** --
 Config.ScreenshotSettings = {
-    encoding = 'jpg',
-    quality = 0.95 -- 0.0 to 1.0
+    encoding = 'webp',
+    quality = 1.0 -- 0.0 to 1.0
 }
 
 -- ** Delay between captures (milliseconds) ** --
 Config.CaptureDelay = 5000 -- 5 seconds between each vehicle (prevents Discord rate limiting)
-
-
